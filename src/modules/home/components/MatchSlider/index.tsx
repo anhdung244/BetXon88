532f1b450844c3a2ce7e33bef033bbd9e807c7bb
anhdung <anhdung@nexondv.com>
2023-03-20 09:37:12 +0700

    add slider match

---
+++ src/modules/home/components/MatchSlider/index.tsx
@@ -0,0 +1,82 @@
+import { Carousel } from "antd";
+import MatchBox from "./MatchBox";
+import "./style.scss";
+
+const responsive = [
+  {
+    breakpoint: 1200,
+    settings: {
+      autoplay: true,
+      infinite: true,
+      draggable: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+    },
+  },
+  {
+    breakpoint: 1400,
+    settings: {
+      autoplay: true,
+      infinite: true,
+      draggable: true,
+      slidesToShow: 3,
+      slidesToScroll: 2,
+    },
+  },
+  {
+    breakpoint: 1980,
+    settings: {
+      autoplay: true,
+      infinite: true,
+      draggable: true,
+      slidesToShow: 4,
+      slidesToScroll: 2,
+    },
+  },
+  {
+    breakpoint: 2100,
+    settings: {
+      autoplay: true,
+      infinite: true,
+      draggable: true,
+      slidesToShow: 5,
+      slidesToScroll: 2,
+    },
+  },
+  {
+    breakpoint: 2500,
+    settings: {
+      autoplay: true,
+      infinite: true,
+      draggable: true,
+      slidesToShow: 6,
+      slidesToScroll: 2,
+    },
+  },
+];
+
+const MatchSlider = () => {
+  return (
+    <div className="match-slider">
+      <Carousel responsive={responsive}>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+        <MatchBox></MatchBox>
+      </Carousel>
+    </div>
+  );
+};
+
+export default MatchSlider;
