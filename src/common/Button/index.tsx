224d148683ef50d17ce917c3bc4bb59d54d7f9e6
anhdung <anhdung@nexondv.com>
2023-03-27 09:59:02 +0700

    add point box

--- src/common/Button/index.tsx
+++ src/common/Button/index.tsx
@@ -1,25 +1,29 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import styles from "./style.module.scss";
 
 interface IButtonProps {
+<<<<<<< HEAD
     type: string;
     children: ReactNode;
     className?: string | null;
+=======
+  type: string;
+  children: ReactNode;
+  className?: string | null;
+>>>>>>> d234a32 (add title and match tab)
 }
 
-function AuthButton({type, children, className, ...props}: IButtonProps) {
-    return (
-        <button
-            className={`${className} ${
-                type === "pinkToGreen"
-                    ? styles["login_btn"]
-                    : styles["logout_btn"]
-            }`}
-            {...props}
-        >
-            {children}
-        </button>
-    );
+function AuthButton({ type, children, className, ...props }: IButtonProps) {
+  return (
+    <button
+      className={`${className} ${
+        type === "pinkToGreen" ? styles["login_btn"] : styles["logout_btn"]
+      }`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default AuthButton;
--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,10 +1,13 @@
 import { Layout } from "antd";
 import MatchSlider from "./components/MatchSlider";
+import NextMatch from "./components/NextMatch";
+import "./style.scss";
 
 const Home = () => {
   return (
-    <Layout.Content>
+    <Layout.Content className="">
       <MatchSlider></MatchSlider>
+      <NextMatch></NextMatch>
     </Layout.Content>
   );
 };
