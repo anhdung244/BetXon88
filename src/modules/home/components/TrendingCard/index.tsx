e793a51382418b95e0317d5a3826b828cb00a32c
lyngocmyphuong <lyngocmyphuong@gmail.com>
2023-03-28 11:18:17 +0700

    push code

---
+++ src/modules/home/components/TrendingCard/index.tsx
@@ -0,0 +1,12 @@
+import {Card} from "antd";
+import "./style.scss";
+
+const TrendingCard = () => {
+  return (
+    <div className="card-container">
+      <Card title="Live Highlights" className="m-[20px]"></Card>
+    </div>
+  );
+};
+
+export default TrendingCard;
--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,10 +1,12 @@
-import { Layout } from "antd";
+import {Layout} from "antd";
 import MatchSlider from "./components/MatchSlider";
+import TrendingCard from "./components/TrendingCard";
 
 const Home = () => {
   return (
     <Layout.Content>
       <MatchSlider></MatchSlider>
+      <TrendingCard></TrendingCard>
     </Layout.Content>
   );
 };
